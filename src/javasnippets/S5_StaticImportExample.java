3fd83bf0e119491c26a2eb9c49bd509976d045c9
Sandeep <samudsan2020@gmail.com>
2023-06-29 17:09:14 +0530

    static import example

---
+++ src/javasnippets/S5_StaticImportExample.java
@@ -0,0 +1,11 @@
+package javasnippets;
+
+import static javasnippets.S6_StaticUtitily.*;
+
+public class S5_StaticImportExample {
+    public static void main(String[] args) {
+        System.out.println(getSalary());
+        System.out.println(basePay);
+        System.out.println(bonus);
+    }
+}
---
+++ src/javasnippets/S6_StaticUtitily.java
@@ -0,0 +1,9 @@
+package javasnippets;
+
+public class S6_StaticUtitily {
+    public static float basePay = 1000000;
+    public static float bonus = 50000;
+    public static float  getSalary(){
+        return basePay + bonus;
+    }
+}
