b1f21a05fa2af49a5d2c81a2bdcca7b7d9d55880
Sandeep <samudsan2020@gmail.com>
2023-11-22 23:38:40 +0530

    two sum problem II when array was sorted

---
+++ src/neetcode/twopointer/S2_TwoSum_II_ArrayIsSorted.java
@@ -0,0 +1,23 @@
+package neetcode.twopointer;
+
+public class S2_TwoSum_II_ArrayIsSorted {
+    public static void main(String[] args) {
+        int nums[]  = {1,2,3,4,7,8,10,23};
+        int target = 15;
+        getTwoSumII(nums, target);
+    }
+
+    public static void getTwoSumII(int nums[], int target){
+        int i = 0, j=nums.length-1;
+        while(i<j){
+            if(nums[i] + nums[j] < target)
+                i++;
+            else if(nums[i] + nums[j] > target)
+                j--;
+            else {
+                System.out.println("i : " + i + "\n" + "j :" + j);
+                break;
+            }
+        }
+    }
+}
