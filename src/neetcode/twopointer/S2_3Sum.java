9fb98ba6809f33b1d20187248f15030f5e498db6
Sandeep <samudsan2020@gmail.com>
2023-11-23 23:08:58 +0530

    3 Sum

--- src/neetcode/twopointer/S2_3Sum.java
+++ src/neetcode/twopointer/S2_3Sum.java
@@ -7,11 +7,9 @@ import java.util.List;
 public class S2_3Sum {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
-
         System.out.println(threeSum(nums));
     }
     public static List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
         int target = 0;
         List<List<Integer>> list = new ArrayList<>();
