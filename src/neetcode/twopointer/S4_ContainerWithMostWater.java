1488db0d7682dc7265714b7c22c91beec6200b4c
Sandeep <samudsan2020@gmail.com>
2023-11-27 07:27:09 +0530

    Container with most water problem implementation

---
+++ src/neetcode/twopointer/S4_ContainerWithMostWater.java
@@ -0,0 +1,25 @@
+package neetcode.twopointer;
+
+public class S4_ContainerWithMostWater {
+    public static void main(String[] args) {
+        int nums[] = {1,8,6,2,5,4,8,3,2};
+        System.out.println(getContainerWithMostWater(nums));
+    }
+
+    public static int getContainerWithMostWater(int nums[]){
+        int left  = 0;
+        int right = nums.length-1;
+        int maxArea = 0, currentArea = 0;
+        while(left < right){
+            int containerLength = right - left;
+            currentArea = containerLength * Math.min(nums[left], nums[right]);
+            maxArea = Math.max(maxArea, currentArea);
+
+            if(nums[left] < nums[right])
+                left++;
+            else
+                right--;
+        }
+        return maxArea;
+    }
+}
