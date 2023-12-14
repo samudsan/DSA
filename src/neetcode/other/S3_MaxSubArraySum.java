3369d90f58ad9cb5c95edf24aa5210c9c7e13be2
Sandeep <samudsan2020@gmail.com>
2023-12-14 09:07:26 +0530

    max sub array sum

---
+++ src/neetcode/other/S3_MaxSubArraySum.java
@@ -0,0 +1,16 @@
+package neetcode.other;
+
+public class S3_MaxSubArraySum {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int soFarMax=0, max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            soFarMax = soFarMax + nums[i];
+            if(max < soFarMax)
+                max = soFarMax;
+            if(soFarMax < 0)
+                soFarMax =0;
+        }
+        System.out.println(max);
+    }
+}
