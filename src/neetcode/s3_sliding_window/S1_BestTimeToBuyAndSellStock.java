baa81d6af1192bad74f7c36b490286c1c71eb5ae
Sandeep Samudrala <samudsan2020@gmail.com>
2024-06-16 23:17:19 +0530

    refactored code

--- src/neetcode/s3_sliding_window/S1_BestTimeToBuyAndSellStock.java
+++ src/neetcode/s3_sliding_window/S1_BestTimeToBuyAndSellStock.java
@@ -8,10 +8,10 @@ public class S1_BestTimeToBuyAndSellStock {
     }
     // this solution is for buying and selling stock only once.
     private static int bestTimeToBuyAndSellStock(int[] prices){
-        int min = Integer.MAX_VALUE;
+        int min = prices[0];
         int profit = 0;
-        for(int i=0; i<prices.length; i++){
-           if(prices[i] <= min)
+        for(int i=1; i<prices.length; i++){
+           if(prices[i] < min)
                min = prices[i];
            else
                profit = Math.max(prices[i] - min, profit);
