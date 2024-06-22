469835797e300f09e0d8aa4e6c2237dd9f2321cc
Sandeep Samudrala <samudsan2020@gmail.com>
2024-06-22 11:17:59 +0530

    refactored code

--- src/neetcode/s3_sliding_window/S2_LongestSubStringWithoutDuplicates.java
+++ src/neetcode/s3_sliding_window/S2_LongestSubStringWithoutDuplicates.java
@@ -11,9 +11,12 @@ Explanation: The answer is "abc", with the length of 3.
 
 public class S2_LongestSubStringWithoutDuplicates {
     public static void main(String[] args) {
-        String s = "xyyzp77xyyzi";
+        String s = "pwwkew";
         int max = 0;
         int p1 = 0, p2 = 0;
+
+        // Hashset does not preserve insertion order, so to remove any element from hashset we need to remove using its elements instead of index
+        // To get hashset elements sorted, we either need to use tree set or add all hashset elements to arraylist then sort arraylist
         HashSet<Character> set = new HashSet<>();
         while (p2 < s.length()) {
             if (!set.contains(s.charAt(p2))) {
@@ -29,7 +32,4 @@ public class S2_LongestSubStringWithoutDuplicates {
     }
 }
 
-
-// Hashset does not preserve insertion order, so to remove any element from hashset we need to remove using its elements instead of index
-// To get hashset elements sorted, we either need to use tree set or add all hashset elements to arraylist then sort arraylist
 
