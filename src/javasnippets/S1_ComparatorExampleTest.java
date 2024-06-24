712c23189994b40d56f8bd72f01af60d251fe388
Sandeep Samudrala <samudsan2020@gmail.com>
2024-06-24 23:35:27 +0530

    refactored code

--- src/javasnippets/S1_ComparatorExampleTest.java
+++ src/javasnippets/S1_ComparatorExampleTest.java
@@ -8,6 +8,7 @@ public class S1_ComparatorExampleTest {
         list.add(2);
         list.add(1);
         list.add(3);
+        // For sort method of collection we need provide list and a comparator class
         Collections.sort(list, new ComparatorExample());
         System.out.println(list);
     }
@@ -17,6 +18,6 @@ class ComparatorExample implements Comparator<Integer>{
 
     @Override
     public int compare(Integer i1 , Integer i2) {
-        return i2.compareTo(i1); // To sort in reverse order just flipped i2 and i1 postions
+        return i1.compareTo(i2); // To sort in reverse order just flipped i2 and i1 postions
     }
 }
