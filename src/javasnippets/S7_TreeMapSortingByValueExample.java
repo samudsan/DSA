800381f48ea2046e6592cc9ecc0554f63e562f3e
Sandeep Samudrala <samudsan2020@gmail.com>
2024-06-25 00:36:33 +0530

    refactored code

--- src/javasnippets/S7_TreeMapSortingByValueExample.java
+++ src/javasnippets/S7_TreeMapSortingByValueExample.java
@@ -15,20 +15,17 @@ public class S7_TreeMapSortingByValueExample {
         treeMap.put("b",808);
         System.out.println("Sorted treeMap based on Keys: "+treeMap); // Keys will be sorted
 
-        //Sorting Using Values, in below Code define list of entry objects and initialised too.
+        // Sorting Using Values, we are storing elements into List, sorting list using collection.sort()
+        // then adding sorted elements into a LinkedHashMap and then to TreeMap
+        // we are using linked hashmap as it preserve order of insertion.
         ArrayList<Map.Entry<String, Integer>> entryArrayList = new ArrayList<>(treeMap.entrySet());
         entryArrayList.sort(Map.Entry.comparingByValue());
         System.out.println(entryArrayList);
         System.out.println("Sorted Arraylist tree map entries : " + entryArrayList);
-
-        //insert arraylist items to new treeMap
-        // we are using linked hashmap as it preserve order of insertion.
         LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
-
         for (Map.Entry<String, Integer> entry: entryArrayList) {
             linkedHashMap.put(entry.getKey(), entry.getValue());
         }
-
         System.out.println("TreeMap sorted by value"+linkedHashMap);
 
 
