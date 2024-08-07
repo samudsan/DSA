33e7c701c18d88bd0811fe32340c2097e90c4c5f
Sandeep Samudrala <samudsan2020@gmail.com>
2024-08-07 23:26:18 +0530

    added binary search program first occurance of a target element

---
+++ src/neetcode/other/S5_IndexOfFirstOccuranceInASortedArray.java
@@ -0,0 +1,27 @@
+package neetcode.other;
+
+public class S5_IndexOfFirstOccuranceInASortedArray {
+    public static void main(String[] args) {
+        int[] arr =  {1,3, 4, 4, 4, 4, 9};
+        int target = 4;
+        System.out.println(firstOccuranceOfTarget(arr, target));
+    }
+
+    public static int firstOccuranceOfTarget(int[] arr, int target){
+        int low = 0;
+        int high = arr.length;
+        int mid, ans = -1;
+        while(low <= high){
+            mid = (low+high)/2;
+            if(arr[mid] == target){
+                ans = mid;
+                high = mid-1;
+            }
+            else if(arr[mid] < target)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return ans;
+    }
+}
