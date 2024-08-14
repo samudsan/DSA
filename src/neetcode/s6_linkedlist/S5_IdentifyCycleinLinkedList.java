2790f487ef2aa1dee814d4be424bdfd4c2ad7c8a
Sandeep Samudrala <samudsan2020@gmail.com>
2024-08-15 00:26:35 +0530

    added program which checks if cycle is present in a Linked List

---
+++ src/neetcode/s6_linkedlist/S5_IdentifyCycleinLinkedList.java
@@ -0,0 +1,31 @@
+package neetcode.s6_linkedlist;
+
+import data_structues.LinkedList.Node;
+
+import static data_structues.LinkedList.LL1_CreateNode.createLLAndGetHead;
+import static data_structues.LinkedList.LL1_CreateNode.traversLinkedList;
+
+public class S5_IdentifyCycleinLinkedList {
+    public static void main(String[] args) {
+        Node head = createLLAndGetHead(new int[]{5,3,4,6,2});
+        traversLinkedList(head);
+        System.out.print(isCyclePresent(head));
+    }
+
+    public static boolean isCyclePresent(Node head){
+        Node current =  head;
+        Node slow = current;
+        Node fast = current.next;
+
+        if (current == null || current.next == null)
+            return false;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) // if cycle available, fast and slow pointer definitely meets
+                return true;
+        }
+        return false;
+    }
+}
