ca616b9c24f4754d891ad3883a44c88668a9cae1
Sandeep Samudrala <samudsan2020@gmail.com>
2024-11-08 11:57:00 +0530

    added comment for better understanding

--- src/neetcode/s3_sliding_window/S1_BestTimeToBuyAndSellStock.java
+++ src/neetcode/s3_sliding_window/S1_BestTimeToBuyAndSellStock.java
@@ -4,16 +4,17 @@ public class S1_BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
 //        int[] prices = {7,1,5,3,6,4};
         int[] prices = {3,1,2,4,0,5};
-        System.out.println(bestTimeToBuyAndSellStock(prices));
+        System.out.println(TimeToBuyAndSellStock(prices));
     }
     // this solution is for buying and selling stock only once.
-    private static int bestTimeToBuyAndSellStock(int[] prices){
+    private static int TimeToBuyAndSellStock(int[] prices){
         int min = prices[0];
         int profit = 0;
+
         for(int i=1; i<prices.length; i++){
            if(prices[i] < min)
                min = prices[i];
-           else
+           else // we will come to else part only if current price is not less then our min price, It helps if our prices are ordered in decreasing. we don't calculate price that time as prices continuously decreasing
                profit = Math.max(prices[i] - min, profit);
         }
         return profit;
